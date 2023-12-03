b2adcd990c621f2c9666bad8741b3a42d896cd63
Shibaken0618 <kent.shibata06@gmail.com>
2023-12-03 17:59:01 -0500

    updated naming

--- SCREW.m
+++ SCREW.m
@@ -1,10 +1,10 @@
 % function that finds the screw motion using xi and angle
 
-function rot = SCREW(xi,theta)
+function screw = SCREW(xi,theta)
     w = xi(4:6); r = xi(1:3);
     I3 = eye(3);
     R = I3 + (SKEW3(w) * sin(theta)) + ...
         (SKEW3(w)^2 * (1 - cos(theta)));
     p = -SKEW3(w) * r;
-    rot = [R, p; zeros(1,3), 1];
+    screw = [R, p; zeros(1,3), 1];
 end
