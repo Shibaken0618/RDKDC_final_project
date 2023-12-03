2bda146d2ac36350958d3bb2842d66cb5d9e8229
sameerkhan7 <sameerkhan@outlook.com>
2023-12-03 17:00:07 -0500

    added all screw values

--- ur5BodyJacobian.m
+++ ur5BodyJacobian.m
@@ -17,14 +17,23 @@ function J = ur5BodyJacobian(q)
             0,0,0,1];
     screw2=[cos(q(2)),0,sin(q(2)),L0
             0,1,0,0
-            -sin(q(2)),0,cos(q(2)),L0 - L0*cos(q(2))
+            -sin(q(2)),0,cos(q(2)),(1 - cos(q(2)))*L0
             0,0,0,1];
-    screw3=[cos(q(3)),0,sin(q(3)),-sin(q(3))*L0
+    screw3=[cos(q(3)),0,sin(q(3)),-sin(q(3))*(L0 + L1)
             0,1,0,0
-            -sin(q(2)),0,cos(q(2)),L0 - L0*cos(q(2))
+            -sin(q(3)),0,cos(q(3)),(1 - cos(q(3)))*(L0 + L1)
             0,0,0,1];
-    screw4= 
-    screw5= 
-    screw6= 
+    screw4=[cos(q(4)),0,sin(q(4)),-sin(q(4))*(L0 + L1 + L2)
+            0,1,0,0
+            -sin(q(4)),0,cos(q(4)),(1 - cos(q(4)))*(L0 + L1 + L2)
+            0,0,0,1];
+    screw5=[cos(q(5)),-sin(q(5)),0,sin(q(5))*L3
+            sin(q(5)),cos(q(5)),0,(1-cos(q(5)))*L3
+            0,0,1,0
+            0,0,0,1];
+    screw6=[cos(q(6)),0,sin(q(6)),-sin(q(6))*(L0 + L1 + L2 + L4)
+            0,1,0,0
+            -sin(q(6)),0,cos(q(6)),(1 - cos(q(6)))*(L0 + L1 + L2 + L4)
+            0,0,0,1];
 
 end
