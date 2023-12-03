4d7f97ce49acbd10b301867e60c8b7aa1c51b355
Shibaken0618 <kent.shibata06@gmail.com>
2023-12-03 16:40:23 -0500

    basic function for calc

---
+++ FINV.m
@@ -0,0 +1,9 @@
+% function for finding inverse of 4x4 homogenous matrix
+
+function invMat = FINV(g)
+    % find new rotation and translation
+    R = transpose(g(1:3, 1:3));
+    p = -R * g(1:3, 4);
+    % concatenate to new matrix
+    invMat = [R, p; zeros(1,3), 1];
+end
