8feba628fdfe4f2f1b095b19f61f5b192375cc99
Shibaken0618 <kent.shibata06@gmail.com>
2023-12-03 19:21:43 -0500

    fixed bugs

--- ur5FwdKin.m
+++ ur5FwdKin.m
@@ -16,17 +16,17 @@ gst0 = [R0, p0; zeros(1,3), 1];
 
 % define twists
 q1 = [0; 0; 0]; w1 = [0; 0; 1];
-xi_1 = [w1; cross(-w1, q1)];
+xi_1 = [cross(-w1, q1); w1];
 q2 = [0; 0; L0]; w2 = [0; 1; 0];
-xi_2 = [w2; cross(-w2, q2)]; 
+xi_2 = [cross(-w2, q2); w2]; 
 q3 = [0; 0; L0 + L1]; w3 = [0; 1; 0];
-xi_3 = [w3; cross(-w3, q3)];
+xi_3 = [cross(-w3, q3); w3];
 q4 = [0; 0; L0 + L1 + L2]; w4 = [0; 1; 0];
-xi_4 = [w4; cross(-w4, q4)];
+xi_4 = [cross(-w4, q4); w4];
 q5 = [0; L3; L0 + L1 + L2]; w5 = [0; 0; 1];
-xi_5 = [w5, cross(-w5, q5)];
+xi_5 = [cross(-w5, q5); w5];
 q6 = [0; L3; L0 + L1 + L2 + L4]; w6 = [0; 1; 0];
-xi_6 = [w6; cross(-w6, q6)]; 
+xi_6 = [cross(-w6, q6); w6]; 
 
 % define screw motion
 g1 = SCREW(xi_1, theta1); g2 = SCREW(xi_2, theta2);
