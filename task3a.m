10a48945348268b26b47b36a089efb38b5051532
Shibaken0618 <kent.shibata06@gmail.com>
2023-12-05 19:18:33 -0500

    new updates

--- task3a.m
+++ task3a.m
@@ -1,21 +1,24 @@
 %% lab 3 function tests
 
 clear; clc; close all;
-
 ur5 = ur5_interface();
-% rosinit("RVIZ");
-q = [0; pi/4; -pi/2; 0; pi/2; 0];
-% q2 = [pi/2; -pi/3; pi/3; pi/3; pi/2; pi/2];
-% q3 = [0, 0, 0, 0, 0, 0]';
-% q4 = [0; pi/6; pi/3; -pi/4; pi/5; pi/6];
 
+% ur5FrwKin function test
+
+% different angles for experimentation
+q = [0; -pi/4; pi/8; 0; pi/8; 0];  % pose 1 (remove comment to use, comment to not use)
+% q = [pi/2; -pi/3; pi/6; pi/6; pi/2; -pi/4]; % pose 2 (remove comment to use, comment to not use) 
+% q = [-pi/4; -pi/5; -pi/4; -pi/3; -pi/4; pi/5]; % pose 3 (remove comment to use, comment to not use)
+
+% create frames and move robot joints
 gst = ur5FwdKin(q);
-
-pause(2);
-tool_frame = tf_frame('base_link','tool_frame',eye(4));
-pause(2);
+I4 = eye(4);
+tool_frame = tf_frame('base_link','tool_frame',I4);
+pause(1);
 tool_frame.move_frame('base_link',gst);
 pause(2);
-time_interval = 10; 
-ur5.move_joints(q, time_interval);
-pause(time_interval);
+ur5.move_joints(q,10);
+pause(2);
+gst_real = tool_frame.read_frame('base_link');
+pause(2);
+disp(gst_real);
