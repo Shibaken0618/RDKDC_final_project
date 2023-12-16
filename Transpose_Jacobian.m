78fae1e6f680aec7ebb7dc9fe9f879c4d6757640
Kangyuan Huang <152816822+Cucumber12138@users.noreply.github.com>
2023-12-16 17:42:00 -0500

    Update Transpose_Jacobian.m

--- Transpose_Jacobian.m
+++ Transpose_Jacobian.m
@@ -24,48 +24,68 @@ alpha = [    0.8686
    -1.4066
    -0.2459];
 
-theta = [    0.5896
-   -1.4436
-    1.6245
-   -1.7837
-   -1.4064
-   -0.2459];
+theta = [0.5896
+-1.4436
+1.6245
+-1.7837
+-1.4064
+-0.2459];
 
-% Define control gain
-lambda = 0.1;
+g_end = ur5FwdKin(theta);
 
-% Define the number of iterations and step size
-numIterations = 100;
-stepSize = 0.01;
-
-% Switch to ros control
-% ur5.swtich_to_ros_control();
 ur5.move_joints(ur5.home, 10);
 pause(10);
-ur5.move_joints(alpha,10);
+
+ur5.move_joints(alpha, 10);
 pause(10);
 
-% Control loop
-for i = 1:numIterations
-    % Calculate current end-effector position
-    currentAngles = ur5.get_current_joints();
-    % currentPos = ur5FwdKin(currentAngles);
+K = 0.1;
 
-    % Calculate end-effector position error
-    error = theta - currentAngles;
+t_step = 1;
+finalerr = 0;
 
-    % Calculate Jacobian matrix
-    J = ur5BodyJacobian(currentAngles);
+q_k = ur5.get_current_joints();
 
-    % Use the transpose Jacobian control law to compute joint velocities
-    deltaQ = lambda * J' * error;
+gst_star = g_end;
 
-    % Update joint angles
-    currentJointAngles = currentAngles + deltaQ;
+p_star = gst_star(1:3, 4);  % The goal translation
+[~, theta_star] = getXi(gst_star);
 
-    % Send joint angles to the robot
-    ur5.move_joints(currentJointAngles,lambda);
-    pause(lambda);
+gst_present = ur5FwdKin(q_k);  % The present rigid body motion matrix
+p_present = gst_present(1:3, 4);
+[~, theta_present] = getXi(gst_present);
+
+exp_xi_k = inv(gst_star)*gst_present;  % Error between the goal point and the start point
+[xi_k, ~] = getXi(exp_xi_k);
+
+while norm(p_present - p_star) >= 0.005 || abs(theta_present - theta_star) >= 15*pi/180
+    if abs(manipulability(ur5BodyJacobian(q_k), 'detjac')) <0.00001
+        finalerr = -1;  %% Abort and return -1
+        break
+    end
+
+    J = ur5BodyJacobian(q_k);
+    TJ = J';
+    q_k1 = q_k - K*t_step*TJ*xi_k;   %% q_k1 represents q_k+1 which is the next point
+    q_k = q_k1;
+
+    ur5.move_joints(q_k, t_step);
+    pause(t_step);
+ 
+    gst_present = ur5FwdKin(q_k);
+    p_present = gst_present(1:3, 4);
+    [~, theta_present] = getXi(gst_present);
+
+    exp_xi_k = inv(gst_star)*gst_present;
+    [xi_k, ~] = getXi(exp_xi_k);
+end
+
+if finalerr == -1
+    warning('Matrix is close to being singular. Aborting.');
+    disp(finalerr);
+else
+    finalerr = norm(p_present - p_star);
+    disp(['The final positional error is ', num2str(finalerr*100), 'cm'])
 end
 
 ur5.move_joints(ur5.home, 10);
