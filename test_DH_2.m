035a7fb9c0966e61e82c109b56dc9add3fcffc58
xwang389 <52319446+wxy108@users.noreply.github.com>
2023-12-17 13:24:24 -0500

    Update test_DH_2.m

--- test_DH_2.m
+++ test_DH_2.m
@@ -1,6 +1,7 @@
 %% Initialize
 clc; clear
 ur5 = ur5_interface;
+num = 5;
 pen_tip_offset1 = [1 0 0 0; 0 1 0 -.049; 0 0 1 .12228; 0 0 0 1];  %% %% Coordinate of pen-tip in tool frame
 pen_tip_offset2 = [1 0 0 0; 0 1 0 .049; 0 0 1 -.12228; 0 0 0 1];
 
@@ -34,8 +35,8 @@ pen_start = g_start * pen_tip_offset1;
 pen_end = g_end * pen_tip_offset1;
 
 [pen_corner1, pen_corner2] = intermediatePointCalc(pen_start,pen_end);
-g_corner1 = pen_corner1 * pen_tip_offset2;
-g_corner2 = pen_corner2 * pen_tip_offset2;
+% g_corner1 = pen_corner1 * pen_tip_offset2;
+% g_corner2 = pen_corner2 * pen_tip_offset2;
 
 ur5.move_joints(ur5.home, 10);
 pause(10)
@@ -46,16 +47,38 @@ q_current = ur5.get_current_joints();
 g_start_now = ur5FwdKin_DH(q_current);
 [dSO3_start,dR3_start] = locationError(g_start,g_start_now);
 
-angles_mid1 = ur5InvKin(g_corner1);
-[~, min_error_i] = min(vecnorm(angles_mid1 - q_current, 1));
-ur5.move_joints(angles_mid1(:, min_error_i), 10);
-pause(10)
+% angles_mid1 = ur5InvKin(g_corner1);
+% [~, min_error_i] = min(vecnorm(angles_mid1 - q_current, 1));
+% ur5.move_joints(angles_mid1(:, min_error_i), 10);
+% pause(10)
 
-q_current = ur5.get_current_joints();
-angles_mid2 = ur5InvKin(g_corner2);
-[~, min_error_i] = min(vecnorm(angles_mid2 - q_current, 1));
-ur5.move_joints(angles_mid2(:, min_error_i), 10);
-pause(10)
+delta_pen1 = (pen_corner1 - pen_start) / num;
+for i = 1:num
+    q_current = ur5.get_current_joints();
+    pen_mid1{i} = pen_start + delta_pen1 * i;
+    angles_mid1 = ur5InvKin(pen_mid1{i} * pen_tip_offset2);
+    [~, min_error_i] = min(vecnorm(angles_mid1 - q_current, 1));  %% Using joints data to find the closest matching kinematic configuration 
+    angles_mid = angles_mid1(:,min_error_i);
+    ur5.move_joints(angles_mid, 2.5);
+    pause(2.5)
+end
+
+% q_current = ur5.get_current_joints();
+% angles_mid2 = ur5InvKin(g_corner2);
+% [~, min_error_i] = min(vecnorm(angles_mid2 - q_current, 1));
+% ur5.move_joints(angles_mid2(:, min_error_i), 10);
+% pause(10)
+
+delta_pen2 = (pen_corner2 - pen_corner1) / num;
+for i = 1:num
+    q_current = ur5.get_current_joints();
+    pen_mid2{i} = pen_corner1 + delta_pen2 * i;
+    angles_mid2 = ur5InvKin(pen_mid2{i} * pen_tip_offset2);
+    [~, min_error_i] = min(vecnorm(angles_mid2 - q_current, 1));  %% Using joints data to find the closest matching kinematic configuration 
+    angles_mid = angles_mid2(:,min_error_i);
+    ur5.move_joints(angles_mid, 2.5);
+    pause(2.5)
+end
 
 ur5.move_joints(angles_end, 10);
 pause(10)
