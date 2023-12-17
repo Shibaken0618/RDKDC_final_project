510cabab222c7c8d4ae823e5e795f64e33dc65e1
xwang389 <52319446+wxy108@users.noreply.github.com>
2023-12-16 20:42:44 -0500

    Update test_DH_2.m

--- test_DH_2.m
+++ test_DH_2.m
@@ -29,33 +29,41 @@ disp(angles_end)
 ur5.swtich_to_ros_control()
 g_start = ur5FwdKin_DH(angles_start);
 g_end = ur5FwdKin_DH(angles_end);
+[g_corner1, g_corner2] = intermediatePointCalc(g_start,g_end);
 
 ur5.move_joints(ur5.home, 10);
 pause(10)
+
 ur5.move_joints(angles_start, 20);
 pause(20)
-
-delta_g_x = g_end(1, 4) - g_start(1,4);
-delta_g_y = g_end(2, 4) - g_start(2,4);
-
 q_current = ur5.get_current_joints();
-g_mid1 = g_start;
-g_mid1(1, 4) = g_mid1(1, 4) + 0.1 * delta_g_x / norm(delta_g_x);
-angles_mid1 = ur5InvKin(g_mid1);
+g_start_now = ur5FwdKin_DH(q_current);
+[dSO3_start,dR3_start] = locationError(g_start,g_start_now);
+
+angles_mid1 = ur5InvKin(g_corner1 * pen_tip_offset2);
 [~, min_error_i] = min(vecnorm(angles_mid1 - q_current, 1));
 ur5.move_joints(angles_mid1(:, min_error_i), 10);
 pause(10)
 
-q_current = ur5.get_current_joints();
-g_mid2 = g_mid1;
-g_mid2(2, 4) = g_end(2, 4);
-angles_mid2 = ur5InvKin(g_mid2);
-[~, min_error_i] = min(vecnorm(angles_mid2 - q_current, 1));
+angles_mid2 = ur5InvKin(g_corner2 * pen_tip_offset2);
+[min_error, min_error_i] = min(vecnorm(angles_mid2 - q_current, 1));
 ur5.move_joints(angles_mid2(:, min_error_i), 10);
 pause(10)
 
 ur5.move_joints(angles_end, 10);
 pause(10)
+q_current = ur5.get_current_joints();
+g_end_now = ur5FwdKin_DH(q_current);
+[dSO3_end,dR3_end] = locationError(g_end,g_end_now);
 
 ur5.move_joints(ur5.home, 10);
 pause(10);
+
+disp('Rotation error of the start point is:')
+disp(dSO3_start)
+disp('Translation error of the start point is:')
+disp(dR3_start)
+disp('Rotation error of the end point is:')
+disp(dSO3_end)
+disp('Translation error of the end point is:')
+disp(dR3_end)
