3292d04e9050acf76656a5ad538474b917dd50ed
Kangyuan Huang <152816822+Cucumber12138@users.noreply.github.com>
2023-12-16 22:07:30 -0500

    Update test_DH_2.m

--- test_DH_2.m
+++ test_DH_2.m
@@ -1,28 +1,14 @@
 %% Initialize
 clc; clear
 ur5 = ur5_interface;
-
-g_start =
-
-    0.9255   -0.3784   -0.0180    0.2531
-   -0.3774   -0.9251    0.0430    0.5958
-   -0.0329   -0.0330   -0.9989    0.3667
-         0         0         0    1.0000
-
-g_end =
-
-    0.9760    0.2142   -0.0393   -0.1302
-    0.2153   -0.9762    0.0254    0.6341
-   -0.0329   -0.0332   -0.9989    0.3668
-         0         0         0    1.0000
-
 pen_tip_offset1 = [1 0 0 0; 0 1 0 -.049; 0 0 1 .12228; 0 0 0 1];  %% %% Coordinate of pen-tip in tool frame
 pen_tip_offset2 = [1 0 0 0; 0 1 0 .049; 0 0 1 -.12228; 0 0 0 1];
 
 %% Teach
+% ur5.swtich_to_ros_control()
 disp('Teach the start point, press any button to continue.');
 % Switch to pendant control
-ur5.switch_to_pendant_control();
+ur5.swtich_to_pendant_control();
 waitforbuttonpress;
 
 % Record the start point
@@ -32,7 +18,7 @@ disp(angles_start)
 
 % Record the end point
 disp('Teach the end point, press any button to continue.');
-ur5.switch_to_pendant_control();
+ur5.swtich_to_pendant_control();
 waitforbuttonpress;
 
 angles_end = ur5.get_current_joints();
@@ -43,7 +29,13 @@ disp(angles_end)
 ur5.swtich_to_ros_control()
 g_start = ur5FwdKin_DH(angles_start);
 g_end = ur5FwdKin_DH(angles_end);
-[g_corner1, g_corner2] = intermediatePointCalc(g_start,g_end);
+
+pen_start = g_start * pen_tip_offset1;
+pen_end = g_end * pen_tip_offset1;
+
+[pen_corner1, pen_corner2] = intermediatePointCalc(pen_start,pen_end);
+g_corner1 = pen_corner1 * pen_tip_offset2;
+g_corner2 = pen_corner2 * pen_tip_offset2;
 
 ur5.move_joints(ur5.home, 10);
 pause(10)
@@ -54,13 +46,14 @@ q_current = ur5.get_current_joints();
 g_start_now = ur5FwdKin_DH(q_current);
 [dSO3_start,dR3_start] = locationError(g_start,g_start_now);
 
-angles_mid1 = ur5InvKin(g_corner1 * pen_tip_offset2);
+angles_mid1 = ur5InvKin(g_corner1);
 [~, min_error_i] = min(vecnorm(angles_mid1 - q_current, 1));
 ur5.move_joints(angles_mid1(:, min_error_i), 10);
 pause(10)
 
-angles_mid2 = ur5InvKin(g_corner2 * pen_tip_offset2);
-[min_error, min_error_i] = min(vecnorm(angles_mid2 - q_current, 1));
+q_current = ur5.get_current_joints();
+angles_mid2 = ur5InvKin(g_corner2);
+[~, min_error_i] = min(vecnorm(angles_mid2 - q_current, 1));
 ur5.move_joints(angles_mid2(:, min_error_i), 10);
 pause(10)
 
