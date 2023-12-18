f762d753cdd14ce1d0a0717c6dc2cd9c58782639
sameerkhan7 <sameerkhan@outlook.com>
2023-12-18 11:47:04 -0500

    added JT control

--- JTControlFunc.m
+++ JTControlFunc.m
@@ -1,19 +1,15 @@
 %% main function for RR Control
 
-function [sp_err,so_err,ep_err,eo_err] = RRControlFunc(ur5, theta_start, theta_end)
+function [sp_err,so_err,ep_err,eo_err] = JTControlFunc(ur5, theta_start, theta_end)
     g_start = ur5FwdKin_DH(theta_start);
     g_end = ur5FwdKin_DH(theta_end);
-
-    [g_corner1, g_corner2] = intermediatePointCalc(g_start,g_end);
 
     ur5.move_joints(theta_start, 15);
     pause(15)
 
     [so_err,sp_err] = locationError(g_start, ur5FwdKin_DH(ur5.get_current_joints()));
     
-    errorc1 = ur5RRcontrol(g_corner1, 0.1, ur5_interface);
-    errorc2 = ur5RRcontrol(g_corner2, 0.1, ur5_interface);
-    error_end = ur5RRcontrol(g_end, 0.05, ur5_interface);
+    error_end = ur5JTcontrol(g_end, 0.05, ur5_interface);
 
     [eo_err,ep_err] = locationError(g_end, ur5FwdKin_DH(ur5.get_current_joints()));
 end
---
+++ ur5JTcontrol.m
@@ -0,0 +1,51 @@
+function finalerr = ur5JTcontrol(gdesired, K, ur5_interface)
+ur5 = ur5_interface;
+
+t_step = 2;
+finalerr = 0;
+
+q_k = ur5.get_current_joints();
+
+gst_star = gdesired;
+
+p_star = gst_star(1:3, 4);  %% The goal translation
+[~, theta_star] = getXi(gst_star);
+
+gst_present = ur5FwdKin_DH(q_k);  %% The present rigid body motion matrix
+p_present = gst_present(1:3, 4);
+[~, theta_present] = getXi(gst_present);
+
+exp_xi_k = inv(gst_star)*gst_present;  %% Error between the goal point and the start point
+[xi_k, ~] = getXi(exp_xi_k);
+
+while norm(p_present - p_star) >= 0.005 || abs(theta_present - theta_star) >= 15*pi/180
+    if abs(manipulability(ur5BodyJacobian(q_k), 'detjac')) <0.00001
+        finalerr = -1;  %% Abort and return -1
+        break
+    end
+
+    q_k1 = q_k - K*t_step*transpose(ur5BodyJacobian(q_k))*xi_k;   %% q_k1 represents q_k+1 which is the next point
+    q_k = q_k1;
+
+    ur5.move_joints(q_k, t_step);
+    pause(t_step)
+
+    gst_present = ur5FwdKin_DH(q_k);
+    p_present = gst_present(1:3, 4);
+    [~, theta_present] = getXi(gst_present);
+    
+    exp_xi_k = inv(gst_star)*gst_present;
+    [xi_k, ~] = getXi(exp_xi_k);
+    disp('Current angles are:')
+    disp(ur5.get_current_joints())
+end
+
+if finalerr == -1
+    warning('Matrix is close to being singular. Aborting.');
+    disp(finalerr);
+else
+    finalerr = norm(p_present - p_star);
+    disp(['The final positional error is ', num2str(finalerr*100), 'cm'])
+end
+
+end
