efb296a14c64371b48d25d69d7e11abc30d7a3df
Sameer Khan <54297501+sameerkhan7@users.noreply.github.com>
2023-12-18 00:10:52 -0500

    Update RRControlFunc.m

--- RRControlFunc.m
+++ RRControlFunc.m
@@ -4,12 +4,9 @@ function RRControlFunc(ur5, theta_start, theta_end)
     g_start = ur5FwdKin_DH(theta_start);
     g_end = ur5FwdKin_DH(theta_end);
 
-    %ur5 = ur5_interface();
-    ur5.move_joints(ur5.home, 15);
-    pause(15);
+    %ur5.move_joints(ur5.home, 15);
+    %pause(15);
     [g_corner1, g_corner2] = intermediatePointCalc(g_start,g_end);
-
-    %theta_start = ur5InvKin(g_start*pen_tip_offset1);
 
     ur5.move_joints(theta_start(:,6), 15);
     pause(15)
