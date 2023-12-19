2da559b4c5845c6a33b9e93ed77b325ba0f7e9fd
xwang389 <52319446+wxy108@users.noreply.github.com>
2023-12-18 21:34:17 -0500

    Update test_DH_3_sim.m

--- test_DH_3_sim.m
+++ test_DH_3_sim.m
@@ -7,18 +7,23 @@ ur5 = ur5_interface;
 % ur5.move_joints(ur5.home, 10);
 % pause(10);
 
-g_start = [ -1   0    0  -0.35
-           0   1    0   0.2
-           0   0   -1   0.5419
-           0   0    0   1];
+% g_start = [ -1   0    0  -0.35
+%            0   1    0   0.2
+%            0   0   -1   0.5419
+%            0   0    0   1];
 
-g_end = [ -1   0    0  -0.2
-           0   1    0   0.3
-           0   0   -1   0.5419
-           0   0    0   1];
+x = pi/2;
+g_start = [cos(x) -sin(x) 0 -0.25;
+           -sin(x) cos(x) 0 0.6;
+           0 0 -1 0;
+           0 0 0 1];
+
+% g_end = [ -1   0    0  -0.2
+%            0   1    0   0.3
+%            0   0   -1   0.5419
+%            0   0    0   1];
 
 point_start = g_start * pen_tip_offset2;
-point_end = g_end * pen_tip_offset2;
 
 %% Display frames
 start_frame = tf_frame('base_link','start',eye(4));  %% Start point frame
@@ -38,18 +43,15 @@ pause(20)
 
 % Record the first point
 angles_start = ur5.get_current_joints();
-% pause(10)
 
 %% Calculates desired point
 g_start = ur5FwdKin_DH(angles_start);
 pen_start = g_start * pen_tip_offset1;
-% ur5.move_joints(angles_start, 20);
-% pause(20)
 
 t_step = 1;
 [x, y] = extra(150);
-x = x.*0.01;
-y = (y - y(1)).*0.01;
+x = x.*0.003;
+y = (y - y(1)).*0.003;
 points = size(x, 2);
 count = 1;
 
