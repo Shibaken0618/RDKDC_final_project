3c319d26c6a4bd4d598802a2dc1199d1d64132c4
Shibaken0618 <kent.shibata06@gmail.com>
2023-12-19 21:43:09 -0500

    updates

--- extra.m
+++ extra.m
@@ -1,5 +1,7 @@
-function [x, y] = extra(num)
+%% function for extra credit tast image
+% draws a 2D heart using mathematical functions to acheive its shape
 
+function [x, y] = extra(num)
 t = linspace (-pi,pi, num); 
 x = 16*sin (t).^3; 
 y = 13*cos (t) - 5*cos (2*t) - 2*cos (3*t) - cos (4*t);
