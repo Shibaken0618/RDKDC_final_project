43717ad84a0b455c56a4e88fb282718714095b85
Sameer Khan <54297501+sameerkhan7@users.noreply.github.com>
2023-12-19 21:42:15 -0500

    Update locationError.m

--- locationError.m
+++ locationError.m
@@ -5,6 +5,7 @@ R_c = g_current(1:3,1:3);
 r_d = g_desired(1:3,4);
 r_c = g_current(1:3,4);
 
+%equations to calculate orientation and positional errors
 dSO3 = sqrt(trace((R_c-R_d)*(R_c-R_d).'));
 dR3 = norm(r_c-r_d);
 end
